20f9dd3d3fc6c8a8557408180a2c4f821e9cbf04
Deepak Patil <108725514+DeepakkPatil@users.noreply.github.com>
2022-11-24 16:11:58 +0530

    [Feat]Card

---
+++ components/Card/Card.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Card.module.scss";
+
+const Card = () => {
+  return(
+    <div className="container">
+
+      <div className="Card-header"><h2>Title</h2></div>
+      <div className="Card-image">
+        <img src="img" alt='image'/>
+      </div>
+      <div className="Card-content">
+       
+       </div>
+    </div>
+  )
+};
+
+export default Card;
