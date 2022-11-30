7845e7d1be6a69e7a787c99aa8bdd556d0114b7d
shishirtiwari23 <shishir456tiwari@gmail.com>
2022-11-30 23:38:52 +0530

    Storybook configured to handle nextjs images

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,13 @@
-import '../styles/globals.css';
+import '../styles/globals.scss';
+
+import * as NextImage from 'next/image';
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized blurDataURL="" />,
+});
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
