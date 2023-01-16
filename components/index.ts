561324923dbffd0bbd5c6e970557502b333123a6
shishirtiwari23 <shishir456tiwari@gmail.com>
2023-01-16 23:43:15 +0530

    refactor: from index.tsx components removed

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,1 @@
 export * from './Buttons';
-export { default as Card } from './Card';
-export { default as Loader } from './Loader';
-export { default as Modal } from './Modal';
-export { default as TextField } from './TextField';
