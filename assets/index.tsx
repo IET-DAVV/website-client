2a592adc275ee6072f346cd56d889458c92878cd
Aakanksha Trivedi <110387985+aakanksha32173@users.noreply.github.com>
2023-02-18 22:39:52 +0530

    Update index.tsx

--- assets/index.tsx
+++ assets/index.tsx
@@ -1,6 +1,8 @@
 //  Importing Icons
 import discord from './icons/discord.svg'
+
 import arrowCircleRight from './icons/arrowCircleRight.svg'
+
 import line from './icons/line.svg'
 export const icons = {
   discord,
