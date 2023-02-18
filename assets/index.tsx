64d7d227d6eb8058dd0c9ce922ac30f93c025235
Aakanksha Trivedi <110387985+aakanksha32173@users.noreply.github.com>
2023-02-18 22:40:24 +0530

    Update index.tsx

--- assets/index.tsx
+++ assets/index.tsx
@@ -4,6 +4,7 @@ import discord from './icons/discord.svg'
 import arrowCircleRight from './icons/arrowCircleRight.svg'
 
 import line from './icons/line.svg'
+
 export const icons = {
   discord,
   arrowCircleRight,
