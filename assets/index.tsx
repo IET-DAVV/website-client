eba0f53747832830cf793f05694ddf0fe7602594
Aakanksha Trivedi <aakankshatrivedi27@gmail.com>
2023-02-23 22:56:23 +0530

    HeroSection made responsive

--- assets/index.tsx
+++ assets/index.tsx
@@ -2,11 +2,15 @@
 import ArrowCircleLeft from './icons/ArrowCircleLeft.svg';
 
 import ArrowCircleRight from './icons/ArrowCircleRight.svg';
+
 import sihwinner from './sihwinners.png';
 
 import discord from './icons/discord.svg';
+
 import arrowCircleRight from './icons/arrowCircleRight.svg'
+
 import line from './icons/line.svg'
+
 export const icons = {
   discord,
   arrowCircleRight,
@@ -14,5 +18,4 @@ export const icons = {
   ArrowCircleRight,
   ArrowCircleLeft,
 };
-
 export const images = { sihwinner };
