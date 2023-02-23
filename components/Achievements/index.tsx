6ba50ef8882efa771ed86ade61018f4e147ddea1
Harsh Farkiya <108735661+HarshhFarkiya@users.noreply.github.com>
2023-02-23 17:08:49 +0530

    ci:Achievement Section Update

--- components/Achievements/index.tsx
+++ components/Achievements/index.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import Slider from 'react-slick';
 
@@ -11,18 +11,15 @@ import styles from './Achievements.module.scss';
 export interface AchievementProps {
   sampleProp: string;
 }
+
 const Achievement: React.FC<AchievementProps> = ({ sampleProp }) => {
   const { ArrowCircleRight, ArrowCircleLeft } = icons;
   const { sihwinner } = images;
+  const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
+  function handleAfterChange(index: number) {
+    setCurrentSlideIndex(index);
+  }
   const [Achievements, setAchievement] = useState([
-    {
-      title: 'sih winners',
-      year: '2022',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus  nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum  tellus elit sed risus',
-      src: sihwinner,
-      url: 'url1',
-    },
     {
       title: 'sih winners',
       year: '2025',
@@ -31,13 +28,24 @@ const Achievement: React.FC<AchievementProps> = ({ sampleProp }) => {
       src: sihwinner,
       url: 'url2',
     },
+    {
+      title: 'sih winners',
+      year: '2022',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus  nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum  tellus elit sed risus',
+      src: sihwinner,
+      url: 'url1',
+    },
   ]);
   const sliderRef = useRef<Slider>(null);
   const handleNext = () => {
     sliderRef.current?.slickNext();
+    //Achievements.length is the size of our array, do not forgot to mention the size of array
+    setCurrentSlideIndex((currentSlideIndex + 1) % Achievements.length);
   };
   const handlePrev = () => {
     sliderRef.current?.slickPrev();
+    setCurrentSlideIndex((currentSlideIndex - 1) % Achievements.length);
   };
   const settings = {
     dots: false,
@@ -46,30 +54,43 @@ const Achievement: React.FC<AchievementProps> = ({ sampleProp }) => {
     slidesToShow: 1,
     slidesToScroll: 1,
     fade: true,
-    ref: sliderRef,
     autoplay: true,
-    arrows: false,
+    adaptiveHeight: true,
+    afterChange: handleAfterChange,
   };
-  const slides = Achievements.map((slide) => {
+  const itemsRef = useRef<Array<HTMLParagraphElement | null>>([]);
+  // you can access the elements with itemsRef.current[n]
+  useEffect(() => {
+    itemsRef.current = itemsRef.current.slice(0, Achievements.length);
+  }, [Achievements]);
+  const slides = Achievements.map((slide, i) => {
+    const showText = () => {
+      itemsRef.current[currentSlideIndex]?.classList.remove(
+        styles.descEllipsis
+      );
+    };
     return (
-      <div className={styles.group3646} key={slide.url}>
+      <div className={styles.group3646} key={i}>
         <div className={styles.achievementinfo}>
-          <div id={styles.year}>
-            <p>{slide.year}</p>
-          </div>
-          <div id={styles.title}>
-            <p>{slide.title}</p>
-          </div>
-          <div id={styles.desc}>
-            <p>{slide.description}</p>
-          </div>
+          <p className={styles.year}>{slide.year}</p>
+          <p className={styles.title}>{slide.title}</p>
+          <p
+            className={`${styles.desc}  ${styles.descEllipsis}`}
+            onClick={() => showText()}
+            ref={(el) => {
+              itemsRef.current[i] = el;
+            }}
+            role="presentation"
+          >
+            {slide.description}
+          </p>
         </div>
         <div className={styles.image120}>
           <Image
             src={slide.src}
             alt="Achievement Image"
-            height="500"
-            width="900"
+            height="450"
+            width="850"
             className={styles.Image}
           />
         </div>
@@ -90,7 +111,9 @@ const Achievement: React.FC<AchievementProps> = ({ sampleProp }) => {
           </button>
         </div>
         <div className={styles.slider}>
-          <Slider {...settings}>{slides}</Slider>
+          <Slider ref={sliderRef} {...settings}>
+            {slides}
+          </Slider>
         </div>
         <div className={styles.btnright}>
           <button onClick={handleNext} type="button">
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app';
 import 'slick-carousel/slick/slick-theme.css';
 import 'slick-carousel/slick/slick.css';
 import '../styles/globals.scss';
+import './global.css';
 // eslint-disable-next-line react/function-component-definition
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
