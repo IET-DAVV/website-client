9726793b2124865122c5c89b3d739acb40627095
Rishi <grishi634@gmail.com>
2023-02-25 15:30:55 +0530

    fix: eslint error fixed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,12 @@ import '../styles/globals.scss';
 import AlertContextProvider from '../utils/contexts/AlertContext';
 
 // eslint-disable-next-line react/function-component-definition
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <AlertContextProvider>
       <Component {...pageProps} />
     </AlertContextProvider>
   );
-}
+};
 
 export default MyApp;
--- utils/contexts/AlertContext.tsx
+++ utils/contexts/AlertContext.tsx
@@ -1,5 +1,6 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useMemo, useState } from 'react';
 import Alert from '../../components/Alert';
+/*global NodeJS */
 
 interface IAlertContext {
   Message: () => {
@@ -38,77 +39,79 @@ const AlertContextProvider: React.FC<AlertContextProviderProps> = ({
 }) => {
   const [Alerts, setAlerts] = useState<Array<IAlert>>([]);
 
-  function Message() {
-    let id = (Math.random() * 100 + 100).toLocaleString();
-    let res = {
-      success: function (
-        message: string,
-        autoClose: boolean = true,
-        duration: number = 1000
-      ) {
-        console.log(message, autoClose, duration);
-        setAlerts((state) => [
-          ...state,
-          {
-            message,
-            autoClose,
-            type: 'success',
-            id,
-          },
-        ]);
-        if (autoClose) {
-          return setTimeout(() => {
-            setAlerts((state) => state.filter((value) => value.id !== id));
-          }, duration);
-        }
-      },
-      error: function (
-        message: string,
-        autoClose: boolean = true,
-        duration: number = 1000
-      ) {
-        console.log(message, autoClose, duration);
-        setAlerts((state) => [
-          ...state,
-          {
-            message,
-            autoClose,
-            type: 'error',
-            id,
-          },
-        ]);
-        if (autoClose) {
-          return setTimeout(() => {
-            setAlerts((state) => state.filter((value) => value.id !== id));
-          }, duration);
-        }
-      },
-      warning: function (
-        message: string,
-        autoClose: boolean = true,
-        duration: number = 1000
-      ) {
-        console.log(message, autoClose, duration);
-        setAlerts((state) => [
-          ...state,
-          {
-            message,
-            autoClose,
-            type: 'warning',
-            id,
-          },
-        ]);
-        if (autoClose) {
-          return setTimeout(() => {
-            setAlerts((state) => state.filter((value) => value.id !== id));
-          }, duration);
-        }
-      },
+  const Message = useMemo(() => {
+    return () => {
+      const id = (Math.random() * 100 + 100).toLocaleString();
+      const res = {
+        success: (
+          message: string,
+          autoClose: boolean = true,
+          duration: number = 1000
+        ) => {
+          setAlerts((state) => [
+            ...state,
+            {
+              message,
+              autoClose,
+              type: 'success',
+              id,
+            },
+          ]);
+          if (autoClose) {
+            return setTimeout(() => {
+              setAlerts((state) => state.filter((value) => value.id !== id));
+            }, duration);
+          }
+          return undefined;
+        },
+        error: (
+          message: string,
+          autoClose: boolean = true,
+          duration: number = 1000
+        ) => {
+          setAlerts((state) => [
+            ...state,
+            {
+              message,
+              autoClose,
+              type: 'error',
+              id,
+            },
+          ]);
+          if (autoClose) {
+            return setTimeout(() => {
+              setAlerts((state) => state.filter((value) => value.id !== id));
+            }, duration);
+          }
+          return undefined;
+        },
+        warning: (
+          message: string,
+          autoClose: boolean = true,
+          duration: number = 1000
+        ) => {
+          setAlerts((state) => [
+            ...state,
+            {
+              message,
+              autoClose,
+              type: 'warning',
+              id,
+            },
+          ]);
+          if (autoClose) {
+            return setTimeout(() => {
+              setAlerts((state) => state.filter((value) => value.id !== id));
+            }, duration);
+          }
+          return undefined;
+        },
+      };
+      return res;
     };
-    return res;
-  }
+  }, []);
   function onClose(id: string) {
-    setAlerts((state) => state.filter((value) => value.id !== id));
+    return setAlerts((state) => state.filter((value) => value.id !== id));
   }
   return (
     <AlertContext.Provider value={{ Message }}>
